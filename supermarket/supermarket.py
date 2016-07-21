971a1b4f27f3a5f2407659717ed3192eddd99c83
zachrickert <zachrickert@gmail.com>
2016-07-21 13:56:41 -0700

    Initial commit for the supermarket program.

---
+++ supermarket/supermarket.py
@@ -0,0 +1,12 @@
+"""A program for the supermarket."""
+
+
+def names():
+    """Function to loop through a series of names."""
+    names = ["Adam", "Alex", "Mariah", "Martine", "Columbus"]
+
+    for name in names:
+        print(name)
+
+
+names()
