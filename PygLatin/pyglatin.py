0fdb1244a5c83ffa9574c8b3330051b4ff493db8
zachrickert <zachrickert@gmail.com>
2016-07-21 10:11:53 -0700

    Check input to see if input was alphabetic.

--- PygLatin/pyglatin.py
+++ PygLatin/pyglatin.py
@@ -17,7 +17,7 @@ def main():
     print ('Welcome to the Pig Latin Translator!')
     original = input("Enter a word: ")
 
-    if len(original) > 0:
+    if len(original) > 0 and original.isalpha():
         print (original)
     else:
         print ("empty")
