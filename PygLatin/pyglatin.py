ef57452e930dfe5019fb3f14ab134b4dc79af4ca
zachrickert <zachrickert@gmail.com>
2016-07-21 09:14:33 -0700

    Add a pyglatin directory and started pyglatin.py.

---
+++ PygLatin/pyglatin.py
@@ -0,0 +1,15 @@
+"""This is a Pig Latin Translator."""
+
+"""Pig Latin is a language game, where the first letter of the word is moved to
+ the end and add "ay."
+So "Python" becomes "ythonpay."
+Here are the steps to write a Pig Latin translator in Python:
+
+* Ask the user to input a word in English.
+* Make sure the user entered a valid word.
+* Convert the word from English to Pig Latin.
+* Display the translation result.
+"""
+
+
+print ('Pig Latin')
