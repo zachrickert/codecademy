068ea88b9c08ae712f2cbee3cea530d8d52a33f4
zachrickert <zachrickert@gmail.com>
2016-07-22 18:31:48 -0700

    Start a student gradebook.

---
+++ student/student.py
@@ -0,0 +1,19 @@
+"""Grade book for students in a class."""
+
+
+def main():
+    """Grade book for students in a class."""
+    lloyd = {"name": "Lloyd",
+             "homework": [],
+             "quizzes": [],
+             "tests": []}
+
+    alice = {"name": "Alice",
+             "homework": [],
+             "quizzes": [],
+             "tests": []}
+
+    tyler = {"name": "Tyler",
+             "homework": [],
+             "quizzes": [],
+             "tests": []}
