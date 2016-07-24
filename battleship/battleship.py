01bfb96499083ac2211b4183ae00f74f04e92a25
zachrickert <zachrickert@gmail.com>
2016-07-24 12:08:07 -0700

    Rows, columns and turns are set by varibles.

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -3,19 +3,23 @@
 from random import randint
 
 
+ROWS = 5
+COLUMNS = 10
+TURNS = 4
+
+
 def main():
     """Battleship game."""
     board = []
-
-    for i in range(5):
-        board.append(["O"] * 5)
+    set_board(board)
 
     ship_row = random_row(board)
     ship_col = random_col(board)
+    print(ship_row, ship_col)
 
-    for turn in range(1, 5):
+    for turn in range(1, TURNS + 1):
         print('\n\n')
-        print('Guess #{} out of 4'.format(turn))
+        print('Guess #{} out of {}'.format(turn, TURNS))
         print('=' * 20)
         print_board(board)
         guess_row = int(input("Guess Row: "))
@@ -25,7 +29,7 @@ def main():
             print ("Congratulations! You sank my battleship!")
             break
         else:
-            if (guess_row >= len(board) or guess_col >= len(board)):
+            if (guess_row >= ROWS or guess_col >= COLUMNS):
                 print("Oops, that's not even in the ocean.")
             elif (board[guess_row][guess_col] == 'X'):
                 print("You guessed that one already.")
@@ -36,6 +40,12 @@ def main():
                     print("Game Over.")
 
 
+def set_board(board):
+    """Set up starting board."""
+    for x in range(ROWS):
+        board.append(["O"] * COLUMNS)
+
+
 def print_board(board):
     """Print the game grid."""
     for row in board:
@@ -44,12 +54,12 @@ def print_board(board):
 
 def random_row(board):
     """Return a random row."""
-    return randint(0, len(board) - 1)
+    return randint(0, ROWS - 1)
 
 
 def random_col(board):
     """Return a random column."""
-    return randint(0, len(board) - 1)
+    return randint(0, COLUMNS - 1)
 
 
 main()
