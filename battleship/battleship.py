dbfdace8630ff66b8ea4eaf84b7a4296d8450d7f
zachrickert <zachrickert@gmail.com>
2016-07-23 20:33:45 -0700

    Print the game board.

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -8,5 +8,13 @@ def main():
     for i in range(5):
         board.append(["O"] * 5)
 
+    print_board(board)
+
+
+def print_board(board):
+    """Print the game grid."""
+    for row in board:
+        print (" ".join(row))
+
 
 main()
