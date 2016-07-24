78dd65082f243c12017eef2e1280218308d91387
zachrickert <zachrickert@gmail.com>
2016-07-24 11:23:24 -0700

    Give guesser 4 guesses to get the correct answer.

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -10,26 +10,30 @@ def main():
     for i in range(5):
         board.append(["O"] * 5)
 
-    print_board(board)
-
     ship_row = random_row(board)
     ship_col = random_col(board)
 
-    guess_row = int(input("Guess Row: "))
-    guess_col = int(input("Guess Col: "))
+    for turn in range(1, 5):
+        print('\n\n')
+        print('Guess #{} out of 4'.format(turn))
+        print('=' * 20)
+        print_board(board)
+        guess_row = int(input("Guess Row: "))
+        guess_col = int(input("Guess Col: "))
 
-    if (guess_row == ship_row and guess_col == ship_col):
-        print ("Congratulations! You sank my battleship!")
-    else:
-        if (guess_row >= len(board) or guess_col >= len(board)):
-            print("Oops, that's not even in the ocean.")
-        elif (board[guess_row][guess_col] == 'X'):
-            print("You guessed that one already.")
+        if (guess_row == ship_row and guess_col == ship_col):
+            print ("Congratulations! You sank my battleship!")
+            break
         else:
-            print ("You missed my battleship!")
-            board[guess_row][guess_col] = 'X'
-
-    print_board(board)
+            if (guess_row >= len(board) or guess_col >= len(board)):
+                print("Oops, that's not even in the ocean.")
+            elif (board[guess_row][guess_col] == 'X'):
+                print("You guessed that one already.")
+            else:
+                print ("You missed my battleship!")
+                board[guess_row][guess_col] = 'X'
+                if turn >= 4:
+                    print("Game Over.")
 
 
 def print_board(board):
